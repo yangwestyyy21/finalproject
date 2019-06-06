a0f1419a27d71de5bceed56ee9b17837d5d84270
Benjamin Peng <pengb1823@my.sduhsd.net>
2019-06-06 15:38:31 +0000

    php

---
+++ calculator.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Calculator</title>
+  </head>
+
+
+  <body>
+
+    <h1>Calculator</h1>
+    <p>Type in the fields and hit go.</p>
+
+    <?php
+       // define variables and set to empty values
+       $arg1 = $arg2 = $arg3 = $arg4 = $output = $retc = "";
+
+       if ($_SERVER["REQUEST_METHOD"] == "POST") {
+         $arg1 = test_input($_POST["arg1"]);
+         $arg2 = test_input($_POST["arg2"]);
+	 $arg3 = test_input($_POST["arg3"]);
+	 $arg4 = test_input($_POST["arg4"]);
+         exec("/usr/lib/cgi-bin/sp1a/calculator " . $arg1 . " " . $arg2 . " " . $arg3 . " " . $arg4, $output, $retc); 
+       }
+
+       function test_input($data) {
+         $data = trim($data);
+         $data = stripslashes($data);
+         $data = htmlspecialchars($data);
+         return $data;
+       }
+    ?>
+
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+      Radius: <input type="text" name="arg1"><br>
+      Height: <input type="text" name="arg2"><br>
+      Shape: <input type="text" name="arg3"><br>
+      What to find (volume or surfacearea): <input type="text" name="arg4"><br>
+      <br>
+      <input type="submit" value="Go!">
+    </form>
+
+    <?php
+       // only display if return code is numeric - i.e. exec has been called
+       if (is_numeric($retc)) {
+         echo "<h2>Your Input:</h2>";
+         echo $arg1;
+         echo "<br>";
+         echo $arg2;
+         echo "<br>";
+       
+         echo "<h2>Program Output (an array):</h2>";
+         foreach ($output as $line) {
+           echo $line;
+           echo "<br>";
+         }
+       
+         echo "<h2>Program Return Code:</h2>";
+         echo $retc;
+       }
+    ?>
+    
+  </body>
+</html>
